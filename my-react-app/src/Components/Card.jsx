1a8b6320eeab7a155016013b975bb479c582059c
Pallavi Priya <pallavipriya4321@gmail.com>
2025-07-01 03:59:18 +0530

    Top rated filter added

--- my-react-app/src/Components/Card.jsx
+++ my-react-app/src/Components/Card.jsx
@@ -1,17 +1,13 @@
+import { CDN_URL } from "../utils/constants";
+
 const Card = (props) => {
   const { resData } = props;
   const { name, cuisines, sla, costForTwo, avgRating, cloudinaryImageId } =
     resData?.info;
   return (
     <>
-      <div className="p-1 w-50 shadow-md max-w-mdm-5 flex-col hover:border rounded">
-        <img
-          className="w-50 h-40"
-          src={
-            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-            cloudinaryImageId
-          }
-        />
+      <div className="p-1 w-50 shadow-md max-w-mdm-5 flex-col relative z-10 transition-all duration-300 transform hover:scale-120 hover:z-20 hover:shadow-xl">
+        <img className="w-50 h-40" src={CDN_URL + cloudinaryImageId} />
         <h2 className="text-xl">{name}</h2>
         <h6 className="text-sm break-words whitespace-normal">
           {cuisines.join(", ")}
