c84fc8c0302a7ec64b2fa52ee5c0c182f9adea28
ankitkumar <ankitkumar5049@gmail.com>
2025-07-02 21:25:56 +0530

    ui enhancement

--- my-react-app/src/Components/Body.jsx
+++ my-react-app/src/Components/Body.jsx
@@ -1,21 +1,21 @@
 import Card from "./Card";
 import resList from "../utils/mockdata";
 import { useState } from "react";
+import Switch from '@mui/material/Switch';
 
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <>
       <div>
-        <div className="flex m-2 flex-row justify-center">
+        <div className="flex mt-5 ml-2 mr-2 flex-row justify-center">
           <input
             className="shadow appearance-none border rounded py-1 px-2 mx-3 text-gray-700 cursor-pointer"
             type="text"
             placeholder="Search"
           />
-          <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded cursor-pointer">
-            Search
-          </button>
+          <Switch {...label} color="secondary" />
           <button
             onClick={(listOfRestaurants) => {
               const filteredResList = resList.filter(
@@ -29,7 +29,7 @@ const Body = () => {
           </button>
         </div>
 
-        <div className="flex flex-row flex-wrap m-1 gap-1">
+        <div className="mt-10 ml-8 mr-8 grid grid-cols-4 gap-2">
           {listOfRestaurants.map((restaurant) => (
             <Card key={restaurant.info.id} resData={restaurant} />
           ))}
