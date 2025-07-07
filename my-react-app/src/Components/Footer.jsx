db09fd7cd787038cdddfec9d993c7b4a75eae0d9
Pallavi Priya <pallavipriya4321@gmail.com>
2025-07-07 19:17:08 +0530

    improved footer

--- my-react-app/src/Components/Footer.jsx
+++ my-react-app/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
       <div className="flex bg-black text-white justify-center mt-4 w-screen">
-        <h4>Made with Love by Pallavi</h4>
+        <h4>Made with ❤️ by Pallavi</h4>
       </div>
     </>
   );
