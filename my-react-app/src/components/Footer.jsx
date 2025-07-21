4f0d7709dd24f9b1c86bde27f753115dcf526fb5
pallavipriya4321 <56637207+pallavipriya4321@users.noreply.github.com>
2025-07-21 23:40:52 +0530

    Revert "Issue - Footer fixed at bottom"
    This reverts commit 7026ce1c51bd30616bccd615d412bd52bfbf3381.

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,12 +1,9 @@
 const Footer = () => {
   return (
     <>
-    <footer>
       <div className="flex bg-black text-white justify-center mt-4 w-screen ">
-          <h4>Made with ❤️ by Pallavi</h4>
+        <h4>Made with ❤️ by Pallavi</h4>
       </div>
-    </footer>
-     
     </>
   );
 };
