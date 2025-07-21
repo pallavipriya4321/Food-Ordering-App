7026ce1c51bd30616bccd615d412bd52bfbf3381
ankitkumar <ankitkumar5049@gmail.com>
2025-07-21 21:41:56 +0530

    Issue - Footer fixed at bottom

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,9 +1,12 @@
 const Footer = () => {
   return (
     <>
+    <footer>
       <div className="flex bg-black text-white justify-center mt-4 w-screen ">
-        <h4>Made with ❤️ by Pallavi</h4>
+          <h4>Made with ❤️ by Pallavi</h4>
       </div>
+    </footer>
+     
     </>
   );
 };
